01681c0dddfaf0a1047e4fa43037662be7891005
MarKus-del <marcos.rodrigues.ft@gmail.com>
2021-05-25 13:05:32 -0300

    aula sobre ranges

---
+++ src/aula-5.js
@@ -0,0 +1,25 @@
+const { alfabeto } = require('./base');
+
+// [abc] = conjunto
+// + = sequencia de um conjunto
+// ^  = nega um conjunto
+// [-] = range
+console.log(alfabeto);
+console.log(alfabeto.match(/[^abc123]+/gi));
+
+// console.log(alfabeto.match(/[0-9]+/g));
+// equivale a
+console.log(alfabeto.match(/\d+/g));
+console.log(alfabeto.match(/\D+/g));
+
+// console.log(alfabeto.match(/[a-zA-Z0-9]+/gi));
+// console.log(alfabeto.match(/[a-z]+/gi));
+// equivale a
+console.log(alfabeto.match(/\w+/g));
+console.log(alfabeto.match(/\W/g));
+
+console.log(alfabeto.match(/[\u00A0-\u00B0]/gi)); // Unicode
+
+// retirando espaços
+console.log(alfabeto.match(/\s+/g));
+console.log(alfabeto.match(/\S+/g));
